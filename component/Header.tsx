dc3680cfb26c9b5a40a89c88da3eb54605aed13d
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2026-02-28 10:03:40 +0500

    working on Home page

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,30 +1,22 @@
-import { images } from '@/constant';
-import React from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { images } from "@/constant";
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const Header = () => {
-    let totalItems = 10;
+const CartButton = () => {
+  let totalItems = 10;
   return (
-    <View className="flex-between flex-row w-full my-5">
-     <View className="flex-start">
-                          <Text className="small-bold text-primary">DELIVER TO</Text>
-                          <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
-                              <Text className="paragraph-bold text-dark-100">Croatia</Text>
-                              <Image source={images.arrowDown} className="size-3" resizeMode="contain" />
-                          </TouchableOpacity>
-                      </View>
-     <TouchableOpacity className="cart-btn" onPress={()=> {}}>
-            <Image source={images.bag} className="size-5" resizeMode="contain" />
-
-            {totalItems > 0 && (
-                <View className="cart-badge">
-                    <Text className="small-bold text-white">{totalItems}</Text>
-                </View>
-            )}
-        </TouchableOpacity>
+      <TouchableOpacity className="cart-btn bg-red-300" >
+        <Image source={images.bag} className="size-90" resizeMode="contain" />
       
-    </View>
-  )
-}
 
-export default Header
+        {totalItems < 0 && (
+          <View className="cart-badge">
+            <Text className="small-bold text-white">{totalItems}</Text>
+          </View>
+        )}
+      </TouchableOpacity>
+  
+  );
+};
+
+export default CartButton;
