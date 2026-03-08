2ef76a313cbdcc4454dcefef448601a07f406a39
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2026-03-08 10:00:21 +0500

    working on auth screen

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform :"com.student.food_ordering",
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId :"69acfc430017312e48f8",
+    userCollection :"user"
+}
