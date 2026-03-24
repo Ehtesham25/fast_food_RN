8ee8d8bed64c860efc5e38859200459920257f48
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2026-03-24 17:58:58 +0500

    refactor the code

--- app/(auth)/signUp.tsx
+++ app/(auth)/signUp.tsx
@@ -1,18 +1,22 @@
 import CustomButton from '@/components/CustomButton'
 import CustomInput from '@/components/CustomInput'
+import { createUser } from '@/lib/appwrite'
 import { Link, router } from 'expo-router'
 import React, { useState } from 'react'
 import { Alert, Text, View } from 'react-native'
 
 const SignUp = () => {
-    const[form, setForm]= useState({email:"", password:""});
+    const[form, setForm]= useState({name:"", email:"", password:""});
     const[isSubmitting, setIsubmitting]= useState(false);
-    const onSubmit = ()=>{
-        if(!form.email || !form.password) return Alert.alert('Error', 'Please enter valid email address & password.');
+
+    const onSubmit = async()=>{
+        const { name, email, password } = form;
+        if(!name || !email || !password) return Alert.alert('Error', 'Please enter valid name, email address & password.');
 
         setIsubmitting(true);
 
-        try {
+        try {            
+            await createUser({email, password, name})
             router.replace("/")
             
         } catch (error:any) {
@@ -25,23 +29,31 @@ const SignUp = () => {
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
             <CustomInput
+                placeholder="Enter your first name"
+                value={form.name}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, name: text }))}
+                label="Name"
+            />
+
+            <CustomInput
                 placeholder="Enter your email"
-                value={''}
-                onChangeText={()=>{}}
+                value={form.email}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
                 label="Email"
                 keyboardType="email-address"
             />
             <CustomInput
                 placeholder="Enter your password"
-                value={''}
-                onChangeText={()=>{}}
+                value={form.password}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
                 label="Password"
                 secureTextEntry={true}
             />
 
             <CustomButton
                 title="Sign Up"
-                onPress={()=>{}}
+                isLoading ={isSubmitting}
+                onPress={onSubmit}
             />
              <View className="flex justify-center mt-5 flex-row gap-2">
                 <Text className="base-regular text-gray-100">
