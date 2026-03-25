42d7a405531c6fa81f76b2de9622b0f50b56fd4a
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2026-03-25 20:47:08 +0500

    refactor the code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,42 @@ import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
+import * as Sentry from "@sentry/react-native";
 import "../globals.css";
 
-export default function RootLayout() {
-  const [fontsLoaded, error] = useFonts({
-    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
-    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
-    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
-    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
-    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
-  });
-  useEffect(()=>{
-    if(error) throw error;
-    if(fontsLoaded) SplashScreen.hideAsync();
+Sentry.init({
+  dsn: "https://37546c26b2bba29075147f918b098600@o4510884428185600.ingest.us.sentry.io/4511046643417089",
 
-  },[error, fontsLoaded])
-  return <Stack screenOptions={{headerShown: false}} />;
-}
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [error, fontsLoaded]);
+  return <Stack screenOptions={{ headerShown: false }} />;
+});
