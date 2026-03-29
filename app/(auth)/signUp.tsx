6c0e0e6fac2a7992598d011053f1a0accf32b88d
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2026-03-29 16:48:36 +0500

    refactor the code

--- app/(auth)/signUp.tsx
+++ app/(auth)/signUp.tsx
@@ -1,70 +1,74 @@
-import CustomButton from '@/components/CustomButton'
-import CustomInput from '@/components/CustomInput'
-import { createUser } from '@/lib/appwrite'
-import { Link, router } from 'expo-router'
-import React, { useState } from 'react'
-import { Alert, Text, View } from 'react-native'
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { createUser } from "@/lib/appwrite";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignUp = () => {
-    const[form, setForm]= useState({name:"", email:"", password:""});
-    const[isSubmitting, setIsubmitting]= useState(false);
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const [isSubmitting, setIsubmitting] = useState(false);
 
-    const onSubmit = async()=>{
-        const { name, email, password } = form;
-        if(!name || !email || !password) return Alert.alert('Error', 'Please enter valid name, email address & password.');
+  const onSubmit = async () => {
+    const { name, email, password } = form;
+    if (!name || !email || !password)
+      return Alert.alert(
+        "Error",
+        "Please enter valid name, email address & password."
+      );
 
-        setIsubmitting(true);
+    setIsubmitting(true);
 
-        try {            
-            await createUser({email, password, name})
-            router.replace("/")
-            
-        } catch (error:any) {
-            Alert.alert('Error', error.message);
-        } finally {
-            setIsubmitting(false);
-        }
-
+    try {
+      await createUser({ email, password, name });
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsubmitting(false);
     }
+  };
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
-            <CustomInput
-                placeholder="Enter your first name"
-                value={form.name}
-                onChangeText={(text) => setForm((prev) => ({ ...prev, name: text }))}
-                label="Name"
-            />
+      <CustomInput
+        placeholder="Enter your first name"
+        value={form.name}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, name: text }))}
+        label="Name"
+      />
 
-            <CustomInput
-                placeholder="Enter your email"
-                value={form.email}
-                onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
-                label="Email"
-                keyboardType="email-address"
-            />
-            <CustomInput
-                placeholder="Enter your password"
-                value={form.password}
-                onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
-                label="Password"
-                secureTextEntry={true}
-            />
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
+        label="Password"
+        secureTextEntry={true}
+      />
 
-            <CustomButton
-                title="Sign Up"
-                isLoading ={isSubmitting}
-                onPress={onSubmit}
-            />
-             <View className="flex justify-center mt-5 flex-row gap-2">
-                <Text className="base-regular text-gray-100">
-                    Do not have an account?
-                </Text>
-                <Link href="/(auth)/signIn" className="base-bold text-primary">
-                    Sign In
-                </Link>
-            </View>
+      <CustomButton
+        title="Sign Up"
+        isLoading={isSubmitting}
+        onPress={onSubmit}
+      />
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Do not have an account?
+        </Text>
+        <Link href="/(auth)/signIn" className="base-bold text-primary">
+          Sign In
+        </Link>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
